8e1ea4d7bb58223d4f1dd730c40af3ec7024966a
Alinka08 <alinazoldubaeva@gmail.com>
2023-06-05 20:28:28 -0500

    created nannies card

--- src/nanniesCard/NanniesCard.jsx
+++ src/nanniesCard/NanniesCard.jsx
@@ -10,61 +10,61 @@ const NanniesCard = () => {
       <div className="nanny-list">
         <div className="nanny-card">
           <div className="images">
-            <img src={nanny1}></img>
+            <img src={nanny1} alt="nanny1"></img>
             {/* <img src={nanny2}></img> */}
           </div>
           <div className="text">
             <h2>Kate Li</h2>
-            <p>
+            <p className="nanny-exp">5 years of experience</p>
+            <p className="nanny-desc">
               Hello, everyone! My name is Kate, and I am thrilled to introduce
-              myself as a nanny. Working with children has always been a passion
-              of mine, and I consider it a privilege to have the opportunity to
-              make a positive impact on their lives.
+              myself as a nanny.I consider it a privilege to have the
+              opportunity to make a positive impact on their lives.
             </p>
           </div>
         </div>
         <div className="nanny-card">
           <div className="images">
-            <img src={nanny2}></img>
+            <img src={nanny2} alt="nanny2"></img>
             {/* <img src={nanny2}></img> */}
           </div>
           <div className="text">
             <h2>Kate Li</h2>
-            <p>
+            <p className="nanny-exp">5 years of experience</p>
+            <p className="nanny-desc">
               Hello, everyone! My name is Kate, and I am thrilled to introduce
-              myself as a nanny. Working with children has always been a passion
-              of mine, and I consider it a privilege to have the opportunity to
-              make a positive impact on their lives.
+              myself as a nanny. I consider it a privilege to have the
+              opportunity to make a positive impact on their lives.
             </p>
           </div>
         </div>
         <div className="nanny-card">
           <div className="images">
-            <img src={nanny1}></img>
+            <img src={nanny1} alt="nanny3"></img>
             {/* <img src={nanny2}></img> */}
           </div>
           <div className="text">
             <h2>Kate Li</h2>
-            <p>
+            <p className="nanny-exp">5 years of experience</p>
+            <p className="nanny-desc">
               Hello, everyone! My name is Kate, and I am thrilled to introduce
-              myself as a nanny. Working with children has always been a passion
-              of mine, and I consider it a privilege to have the opportunity to
-              make a positive impact on their lives.
+              myself as a nanny. I consider it a privilege to have the
+              opportunity to make a positive impact on their lives.
             </p>
           </div>
         </div>
         <div className="nanny-card">
           <div className="images">
-            <img src={nanny2}></img>
+            <img src={nanny2} alt="nanny4"></img>
             {/* <img src={nanny2}></img> */}
           </div>
           <div className="text">
             <h2>Kate Li</h2>
-            <p>
+            <p className="nanny-exp">5 years of experience</p>
+            <p className="nanny-desc">
               Hello, everyone! My name is Kate, and I am thrilled to introduce
-              myself as a nanny. Working with children has always been a passion
-              of mine, and I consider it a privilege to have the opportunity to
-              make a positive impact on their lives.
+              myself as a nanny. I consider it a privilege to have the
+              opportunity to make a positive impact on their lives.
             </p>
           </div>
         </div>
