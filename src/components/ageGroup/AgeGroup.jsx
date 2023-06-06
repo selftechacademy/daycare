59f47aab0a119b97e54a11b4c1b7b35848edc4d7
orshah <orshpov@gmail.com>
2023-06-05 19:42:30 -0500

    added info

--- src/components/ageGroup/AgeGroup.jsx
+++ src/components/ageGroup/AgeGroup.jsx
@@ -18,41 +18,50 @@ import GlobalStyles from "@mui/material/GlobalStyles";
 import Container from "@mui/material/Container";
 
 const tiers = [
+  //   {
+  //     title: "Infant",
+  //     ageGroupPic: "Picture",
+  //     description: [
+  //       "The relationship that your infant will form with the nurturing caregivers at Hamilton Heights will contribute to a strong sense of safety and security that is the foundation for a healthy, happy childhood. Our infant caregivers are not only top-notch nurturers, they are also experts in infant development. They work hard to support your child's individual milestones and create hands-on sensory experiences that encourage babies to grow and learn.",
+  //     ],
+  //     buttonText: "Want more info?",
+  //     buttonVariant: "outlined",
+  //   },
   {
-    title: "Free",
-    price: "0",
+    title: "Toddler",
+    ageGroupPic: "Picture",
     description: [
-      "10 users included",
-      "2 GB of storage",
-      "Help center access",
-      "Email support",
+      "Our toddler program focuses on nurturing each child's individual growth and encouraging them to make new discoveries. Toddlers at Hamilton Heights learn self-help skills, build language, and begin to share, follow directions, and learn from others. Each day is filled with movement, play and fun! Our toddler teachers are trained experts in helping toddlers grow and learn. Teachers plan daily activities that engage each child's natural sense of curiosity to gather information about their world.",
     ],
-    buttonText: "Sign up for free",
+    buttonText: "Want more info?",
     buttonVariant: "outlined",
   },
   {
-    title: "Pro",
-    subheader: "Most popular",
-    price: "15",
+    title: "Pre-School",
+    ageGroupPic: "Picture",
     description: [
-      "20 users included",
-      "10 GB of storage",
-      "Help center access",
-      "Priority email support",
+      "Our preschool focuses on the development of the whole child- physical, intellectual, social and emotional. The classroom curriculum is child-centered, and teachers design lesson plans based on the actual interests of the children. Each day your preschooler will spend time in large group interactions, small group interactions, and individual child choice time. The classroom environment is set up in learning centers like blocks, dramatic play, art, library, discovery, sand and water, music and movement. Preschoolers at Hamilton Heights play and explore outdoors everyday on our playgrounds and in our natural classrooms.",
     ],
-    buttonText: "Get started",
-    buttonVariant: "contained",
+    buttonText: "Want more info?",
+    buttonVariant: "outlined",
   },
   {
-    title: "Enterprise",
-    price: "30",
+    title: "Pre-Kindergarten",
+    ageGroupPic: "Picture",
     description: [
-      "50 users included",
-      "30 GB of storage",
-      "Help center access",
-      "Phone & email support",
+      "Our Pre-K program is designed for children who will be attending kindergarten the following fall. Our Pre-K teachers are professional educators that help your child become kindergarten-ready — emotionally, socially and academically. Our Pre-K curriculum is child-centered, and teachers design lesson plans based on the actual interests of the children. Each day your preschooler will spend time in large group interactions, small group interactions, and individual child choice time. The classroom environment is set up in learning centers like building, dramatic play, art, library, discovery, sand and water, music and movement. Pre-kindergarteners at Hamilton Heights play and explore outdoors everyday on our playgrounds and in our natural classrooms.",
     ],
-    buttonText: "Contact us",
+    buttonText: "Want more info?",
+    buttonVariant: "outlined",
+  },
+  {
+    title: "School Age",
+    ageGroupPic: "Picture",
+    description: [
+      "For school age children, we offer before and after school programming during the school year as well as a full day, action-packed summer program. Our before and after school program complements the school day by offering a variety of relaxing and recreational activities, as well as a snack and the opportunity to work on homework. Hamilton Heights also offers bus transportation to and from designated schools throughout the area." +
+        "During the summer, school age children engage in activities and clubs that are determined by their interests. Teachers then help cultivate these interests by planning engaging experiences, games, and hands-on activities. These activities may include cooking, woodworking, science, art, math, dramatic play, and sports. In addition, our schoolagers go on weekly field trips all summer long",
+    ],
+    buttonText: "Want more info?",
     buttonVariant: "outlined",
   },
 ];
@@ -80,7 +89,7 @@ function AgeGroup() {
           color="text.primary"
           gutterBottom
         >
-          Age groups
+          Infant to School Age Programs
         </Typography>
         <Typography
           variant="h5"
@@ -88,9 +97,10 @@ function AgeGroup() {
           color="text.secondary"
           component="p"
         >
-          Quickly build an effective pricing table for your potential customers
-          with this layout. It&apos;s built with default MUI components with
-          little customization.
+          At Helen Lucy Daycare, we believe that every child is unique and
+          deserves the best care possible. That's why we offer personalized
+          attention, flexible schedules, and affordable rates. Whether you need
+          full-time or part-time care, we have a spot for your child.
         </Typography>
       </Container>
       {/* End hero unit */}
@@ -108,12 +118,7 @@ function AgeGroup() {
               <Card>
                 <CardHeader
                   title={tier.title}
-                  subheader={tier.subheader}
                   titleTypographyProps={{ align: "center" }}
-                  action={tier.title === "Pro" ? <StarIcon /> : null}
-                  subheaderTypographyProps={{
-                    align: "center",
-                  }}
                   sx={{
                     backgroundColor: (theme) =>
                       theme.palette.mode === "light"
@@ -132,13 +137,10 @@ function AgeGroup() {
                   >
                     <Typography
                       component="h2"
-                      variant="h3"
+                      variant="h4"
                       color="text.primary"
                     >
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="text.secondary">
-                      /mo
+                      {tier.ageGroupPic}
                     </Typography>
                   </Box>
                   <ul>
