8bd43e9ef3e62999f0186aba8256669fdbcd5c8d
zloibala <boronova25@gmail.com>
2023-06-06 19:48:48 -0500

    Final Result

--- src/components/whyUsSection/WhyUsSection.jsx
+++ src/components/whyUsSection/WhyUsSection.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 import "./whyUsSection.style.css";
-import teddyBear from "./assets/teddy-bear.svg";
-import food from "./assets/diet.svg";
-import blocks from "./assets/abc-block.svg";
-import playTime from "./assets/playtime.svg";
-import happy from "./assets/happy.svg";
+import tiger from "./assets/tigerMama.svg";
+import jellyfish from "./assets/jellyFish.svg";
+import penguin from "./assets/penguinBabe.svg";
+import elephant from "./assets/elephantPapa.svg";
 
 const WhyUsSection = () => {
   return (
     <div className="container">
       <div className="header">
+        <div class="animal"></div>
         <h1 className="header-text">Why you should choose US?</h1>
-        <h3>
-          <img src={happy} alt="" width="70px" height="70px" />
+        <h4>
           From the moment children take their first steps in any of our centers.
-        </h3>
+        </h4>
       </div>
       <div className="info-container">
-        {/* <div className="INFO"> */}
         <div className="item item1">
-          <img src={blocks} alt="" width="30px" height="30px" />
-          <h3 className="items-text"> State-of-The-Art-Facilities</h3>
+          <img src={penguin} alt="" width="40px" height="40px" />
+          <h3 className="items-text-main"> State-of-The-Art-Facilities</h3>
           <p className="items-text">
             Helen Lusys classrooms feature the best amenities. From the
             materials we use to construct our centers to childrens furniture
@@ -28,15 +26,15 @@ const WhyUsSection = () => {
           </p>
         </div>
         <div className="item item2">
-          <img src={teddyBear} alt="" width="30px" height="30px" />
-          <h3 className="items-text"> Community-Based Care </h3>
+          <img src={jellyfish} alt="" width="40px" height="40px" />
+          <h3 className="items-text-main"> Community-Based Care </h3>
           <p className="items-text">
             As a family-owned business, Helen Lusy believes that every Helen’s
             Lusy Center is an extension of family.
           </p>
         </div>
         <div className="item item3">
-          <img src={playTime} alt="" width="30px" height="30px" />
+          <img src={elephant} alt="" width="40px" height="40px" />
           <h3 className="items-text">Our Team</h3>
           <p className="items-text">
             From our directors and teachers, to teachers aides, we employ staff
@@ -45,20 +43,17 @@ const WhyUsSection = () => {
           </p>
         </div>
         <div className="item item4">
-          <img src={food} alt="" width="30px" height="30px" />
+          {/* <img src={} alt="" width="30px" height="30px" /> */}
+          <img src={tiger} alt="" width="40px" height="40px" />
           <h3 className="items-text">Children’s Land Difference</h3>
           <p className="items-text">
             Fresh, organic foods, catered daily State of the art facilities Fine
             arts classes led by specialists Them atic Units with increasing
             exposure to STEAM activities
           </p>
-          <script src="/areas/kindercare/assets/dist/js/main.7bf52372.js">
-            {" "}
-          </script>
         </div>
       </div>
     </div>
-    // </div>
   );
 };
 
